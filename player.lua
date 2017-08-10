2b96a7b6ccd23d784eeb1979c582ffedada3e41f
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-08-10 15:22:39 -0300

    Adiciona colisão com borda da janela
    - Adiciona colisão com borda da janela
    - Altera o player para um objeto circular

--- player.lua
+++ player.lua
@@ -2,9 +2,8 @@ Player = Object:extend()
 function Player:new()
   self.x = 0
   self.y = 0
-  self.width = 30
-  self.height = 30
-  self.speed = 200
+  self.radius = 15
+  self.speed = 800
   self.actions = {
     w = function (dt) self.y = self.y - self.speed * dt end,
     s = function (dt) self.y = self.y + self.speed * dt end,
@@ -13,14 +12,34 @@ function Player:new()
   }
 end
 
+function Player.fire()
+
+end
+
 function Player:update(dt)
   for key, action in pairs(self.actions) do
     if love.keyboard.isDown(key) then
       action(dt)
     end
   end
+
+
+  window_width = love.graphics.getWidth() - self.radius
+  if self.x < self.radius then
+    self.x = self.radius
+  elseif self.x > window_width then
+    self.x = window_width
+  end
+
+  window_height = love.graphics.getHeight() - self.radius
+  if self.y < self.radius then
+    self.y = self.radius
+  elseif self.y > window_height then
+    self.y = window_height
+  end
+
 end
 
 function Player:draw()
-  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+  love.graphics.circle("fill", self.x, self.y, self.radius)
 end
