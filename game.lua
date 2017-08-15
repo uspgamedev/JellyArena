e3f948d4aa384af86b5c4f8aae22ede5c4b1f69d
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-08-15 15:38:25 -0300

    Add simple enemy
    Add a fixed simple enemy

--- game.lua
+++ game.lua
@@ -3,6 +3,7 @@ Game = Object:extend()
 function Game:new()
   require "player"
   require "bullet"
+  require "enemy"
   require "message"
   require "hud"
   self.player = Player()
@@ -10,15 +11,21 @@ function Game:new()
   self.music = love.audio.newSource("test.ogg")
   self.message = Message()
   self.HUD = HUD()
+  self.player = Player()
+  self.bullets = {}
+  self.enemy = Enemy()
 end
 
 function Game:update(dt)
   self.player:update(self, dt)
+
   if (self.player.hp == 0) then
     self.player.speed =  0
     self.player.bulletSpeed = 0
-    self.message.text = "Você morreu! huehuehue\n Tentar novamente?"
+    self.message.text = "Você morreu!"
   end
+
+  self.enemy:update(self, dt)
   for i, b in ipairs(self.bullets) do
     if(b:update(self, dt)) then
       table.remove(self.bullets, i)
@@ -30,6 +37,7 @@ end
 
 function Game:draw()
   self.player:draw()
+  self.enemy:draw()
   for _, b in ipairs(self.bullets) do
     b:draw()
   end
