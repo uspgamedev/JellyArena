55081f7147fd3d7c233c285ecb7b3c42d4d73a3b
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-08-22 14:44:39 -0300

    Refactor stage bound check
    Rename `checkWindowLimit` to `clampToStageBounds`

--- movementSubsystem.lua
+++ movementSubsystem.lua
@@ -9,5 +9,25 @@ function MovementSubsystem:update(game, dt)
     -- TODO: update velocities
 
     e.position = e.position + e.velocity * dt;
+    self.clampToStageBounds(e)
+  end
+end
+
+
+function MovementSubsystem.clampToStageBounds(entity)
+  -- TODO: get stage bounds instead of window bounds
+  stage_width = love.graphics.getWidth() - entity.radius
+  stage_height = love.graphics.getHeight() - entity.radius
+
+  if entity.position.x < entity.radius then
+    entity.position.x = entity.radius
+  elseif entity.position.x > stage_width then
+    entity.position.x = stage_width
+  end
+
+  if entity.position.y < entity.radius then
+    entity.position.y = entity.radius
+  elseif entity.position.y > stage_height then
+    entity.position.y = stage_height
   end
 end
--- player.lua
+++ player.lua
@@ -4,6 +4,6 @@ Player = Object:extend()
 function Player:new()
   self.position = Vector(love.graphics.getWidth()/2, love.graphics.getHeight()/2)
   self.velocity = Vector(0, 0)
-  self.speed = 150
+  self.speed = 500
   self.radius = 20
 end
