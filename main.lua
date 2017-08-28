d909f2c7eec3bc801c929d732e5e3e7f58c69cce
Luan Torres <luantorres95@hotmail.com>
2017-08-28 15:11:06 -0300

    Add main loop description

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ lovetoys.initialize({
   globals = true
 })
 
--- components
+--- components
 require "IsPlayer"
 require "IsEnemy"
 require "Position"
@@ -14,12 +14,12 @@ require "Velocity"
 require "Combat"
 require "Projectile"
 
--- models
-Player = require "Player"
-Enemy = require "Enemy"
-Bullet = require "Bullet"
+--- models
+Player  = require "Player"
+Enemy   = require "Enemy"
+Bullet  = require "Bullet"
 
--- systems
+--- systems
 PlayerInputSystem   = require "PlayerInputSystem"
 EnemyAISystem       = require "EnemyAISystem"
 MovementSystem      = require "MovementSystem"
@@ -31,12 +31,33 @@ function love.load()
   engine = Engine()
   eventmanager = EventManager()
 
-  engine:addSystem(PlayerInputSystem())
-  engine:addSystem(EnemyAISystem())
-  engine:addSystem(MovementSystem())
-  engine:addSystem(CombatSystem())
-  engine:addSystem(DrawSystem())
-  engine:addSystem(HudDrawSystem())
+  --Process input
+  engine:addSystem(PlayerInputSystem(), "update")
+    -- Update player vars and state
+    -- Go to menu
+  -- process enemy AI
+  engine:addSystem(EnemyAISystem(), "update")
+    -- Based on current game state
+    -- Based on player current input <-- cheating AI
+  -- Process movement
+  engine:addSystem(MovementSystem(), "update")
+    -- Update velocity
+    -- Update position
+    -- Update collision groups
+  -- Process collisions
+    -- Find all colliding pairs
+    -- Process each pair (maybe use callbacks for collision response, like play sound, die, etc)
+      -- If we 'delete' something, invalidade all remaining collisions for that body
+      -- If not, just separate both bodies (may generate new collisions, not that important)
+  -- Update statistics (collision response can also change statistics)
+  -- Update animations & visual effects
+  -- Do clean up
+  -- Display
+  engine:addSystem(DrawSystem(), "draw")
+  engine:addSystem(HudDrawSystem(), "draw")
+
+-- ???
+  engine:addSystem(CombatSystem(), "update")
 
   engine:addEntity(Player(love.graphics.getWidth()/2, love.graphics.getHeight()/2))
   engine:addEntity(Enemy(300, 300))
