ad8b8d635b8426604c7538172d09ce90ffabcd35
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-11-07 16:57:45 -0200

    Remove LifeTime from DamageArea
    And add garbagelist to player area attack

--- entities/DamageArea.lua
+++ entities/DamageArea.lua
@@ -1,16 +1,13 @@
-local Position, Circle, Color, Collider, Damage, LifeTime, Follow =
-  Component.load({"Position", "Circle", "Color", "Collider", "Damage", "LifeTime", "Follow"})
+local Position, Circle, Color, Collider, Damage, Follow =
+  Component.load({"Position", "Circle", "Color", "Collider", "Damage", "Follow"})
 
 function createDamageArea(position, radius, damage, parent, followParent)
   local entity = Entity(parent)
-  local lifeTime = LifeTime(0.1, entity)
   entity:add(Position(position.x, position.y))
   entity:add(Color(255, 0, 0))
   entity:add(Circle(radius))
   entity:add(Damage(damage))
   entity:add(Collider("DamageArea", false))
-  entity:add(lifeTime)
-  lifeTime:start()
   if(followParent) then
     entity:add(Follow(parent))
   end
