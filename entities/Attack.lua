8e26a16d587b6a081f32fe51a7d960a7db773f4c
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-11-16 17:21:22 -0200

    Fix attack stats

--- entities/Attack.lua
+++ entities/Attack.lua
@@ -20,7 +20,7 @@ function DefaultAttackConstructors.MeleeAttack(parent)
   entity:add(Label("MeleeAttack"))
   entity:add(Timer(stats:getShotDelay()))
   entity:add(AttackProperties(0))
-  entity:add(Damage(2 * stats.damage))
+  entity:add(Damage(stats.damage))
   entity:add(AttackRange(2))
   return entity
 end
@@ -40,9 +40,9 @@ function DefaultAttackConstructors.DashAttack(parent)
   local entity = Entity(parent)
   local stats = parent:get("Stats")
   entity:add(Label("DashAttack"))
-  entity:add(Timer(2 * stats:getShotDelay()))
+  entity:add(Timer(stats:getShotDelay()))
   entity:add(AttackProperties(8))
-  entity:add(Damage(1))
+  entity:add(Damage(stats.damage))
   entity:add(AttackRange(stats:getShotRange()))
   return entity
 end
