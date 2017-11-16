750eac6cd4abd0cc89569e5a716d9b8f64bb1b17
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-11-16 18:26:19 -0200

    Revert "Add cheat to enemy range"
    This reverts commit b6728ec520597505e520c9b6aaad34d4b8d0812d.

--- systems/ai/Prerequisites.lua
+++ systems/ai/Prerequisites.lua
@@ -8,6 +8,7 @@ function Prerequisites.InAttackRange(action, prerequisite, agent, target, dt)
     local targetPosition = target:get("Position")
 
     local distance = (agentPosition:toVector() - targetPosition:toVector()):len()
+    local distance = distance - agent:get("Circle").radius - target:get("Circle").radius
     if(distance < 0) then
       distance = 0
     end
@@ -24,6 +25,7 @@ function Prerequisites.InDangerRange(action, prerequisite, agent, target, dt)
   local targetPosition = target:get("Position")
 
   local distance = (agentPosition:toVector() - targetPosition:toVector()):len()
+  local distance = distance - agent:get("Circle").radius - target:get("Circle").radius
   if (distance < 200) then
     return true
   end
